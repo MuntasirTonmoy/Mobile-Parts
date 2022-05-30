47d252566058f1659cf58254172ff37361d38d65
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-05-30 18:34:11 +0600

    dashboard feature complete

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Outlet, useSearchParams } from "react-router-dom";
 import CustomLink from "../Utilities/CustomLink";
 import { AiOutlinePlus, AiFillShopping } from "react-icons/ai";
 import { FaUserCircle } from "react-icons/fa";
+import { MdDashboardCustomize } from "react-icons/md";
 import { MdReviews } from "react-icons/md";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
@@ -22,55 +23,46 @@ const Dashboard = () => {
         <div className="drawer-content">
           {(show || navShow) && (
             <>
-              <div>
-                <h1 className="text-5xl font-serif text-center mt-10 mb-3 text-primary font-bold">
-                  Dashboard
-                </h1>
-                <div className="lg:mx-10 mx-6  mt-10 mb-10 grid lg:grid-cols-3 grid-cols-1 gap-5">
-                  <div class="card max-w-lg bg-base-100 shadow-xl">
-                    <figure>
-                      <img
-                        src="https://api.lorem.space/image/shoes?w=400&h=225"
-                        alt="Shoes"
-                      />
-                    </figure>
-                    <div class="card-body">
-                      <h2 class="card-title">Shoes!</h2>
-                      <p>If a dog chews shoes whose shoes does he choose?</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+              <div className="lg:flex items-center justify-center  lg:h-[calc(100vh-200px)]">
+                <div>
+                  <h1 className="text-5xl font-serif text-center mb-3 mt-10 text-primary font-bold">
+                    Dashboard
+                  </h1>
+
+                  <div className="mt-10 text-3xl lg:mx-10 mx-6  mb-10 grid lg:grid-cols-3 grid-cols-1 lg:gap-10 text-center">
+                    <CustomLink to="/dashboard/myProfile">
+                      <div
+                        onClick={onClick}
+                        className="p-10 w-sm shadow rounded-3xl hover:shadow-2xl transition-shadow ease-out duration-200 uppercase"
+                      >
+                        <div>
+                          <FaUserCircle className="text-primary text-7xl mx-auto mb-4"></FaUserCircle>
+                        </div>
+                        My Profile
                       </div>
-                    </div>
-                  </div>
-                  <div class="card max-w-lg bg-base-100 shadow-xl">
-                    <figure>
-                      <img
-                        src="https://api.lorem.space/image/shoes?w=400&h=225"
-                        alt="Shoes"
-                      />
-                    </figure>
-                    <div class="card-body">
-                      <h2 class="card-title">Shoes!</h2>
-                      <p>If a dog chews shoes whose shoes does he choose?</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                    </CustomLink>
+                    <CustomLink to={`/dashboard/myOrders/${user?.email}`}>
+                      <div
+                        onClick={onClick}
+                        className="lg:mt-0 mt-10 p-10 w-sm shadow rounded-3xl hover:shadow-2xl transition-shadow ease-out duration-200 uppercase"
+                      >
+                        <div>
+                          <AiFillShopping className="text-primary text-7xl mx-auto mb-4"></AiFillShopping>
+                        </div>
+                        My Order
                       </div>
-                    </div>
-                  </div>
-                  <div class="card max-w-lg bg-base-100 shadow-xl">
-                    <figure>
-                      <img
-                        src="https://api.lorem.space/image/shoes?w=400&h=225"
-                        alt="Shoes"
-                      />
-                    </figure>
-                    <div class="card-body">
-                      <h2 class="card-title">Shoes!</h2>
-                      <p>If a dog chews shoes whose shoes does he choose?</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                    </CustomLink>
+                    <CustomLink to="/dashboard/addReview">
+                      <div
+                        onClick={onClick}
+                        className="lg:mt-0  mt-10 p-10 w-sm shadow rounded-3xl hover:shadow-2xl transition-shadow ease-out duration-200 uppercase"
+                      >
+                        <div>
+                          <MdReviews className="text-primary text-7xl mx-auto mb-4"></MdReviews>
+                        </div>
+                        Add Review
                       </div>
-                    </div>
+                    </CustomLink>
                   </div>
                 </div>
               </div>
@@ -98,7 +90,8 @@ const Dashboard = () => {
               className="py-2 text-lg font-bold"
             >
               <CustomLink to="/dashboard/">
-                <FaUserCircle className="text-2xl"></FaUserCircle>Dashboard
+                <MdDashboardCustomize className="text-2xl"></MdDashboardCustomize>
+                Dashboard
               </CustomLink>
             </li>
             <hr />
